90704dfb43794124b0bb15172c066cd573b9aadc
YuSuBui <buicongsum2001@gmail.com>
2022-01-26 13:41:56 +0700

    [H] update rotating group

--- src/app/core/MainGroup.ts
+++ src/app/core/MainGroup.ts
@@ -12,6 +12,7 @@ export class MainGroup implements IGroup {
 
     constructor() {
         this.group = new Object3D();
+        this.group.rotateX(Math.PI / 2);
     }
 
     getPickableNodes(): IGraphic[] {
